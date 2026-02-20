b960d957ecf6d9d949c6fb9339b1fe0f7da99057
Evan R <ecrovelli@gmail.com>
2026-02-19 20:57:28 -0500

    sql workbook for merging medicaid.provider_spending_staging and nppes.npi_staging

---
+++ workbook.sql
@@ -0,0 +1,77 @@
+SELECT
+    hcpcs_code,
+    SUM(claims_count)        AS total_claims_count,
+    SUM(total_amount_paid)   AS total_amount_paid
+FROM medicaid.provider_spending_staging
+WHERE hcpcs_code LIKE 'D%'
+GROUP BY hcpcs_code
+ORDER BY total_claims_count DESC;
+
+
+SELECT
+    p.*,
+    n.provider_first_line_business_practice_location_address,
+	n.provider_second_line_business_practice_location_address,
+	n.provider_business_practice_location_address_city_name,
+	n.provider_business_practice_location_address_state_name, 
+	n.provider_business_practice_location_address_postal_code, 
+	n.provider_business_practice_location_address_country_code, 
+	n.provider_business_practice_location_address_telephone_number
+FROM medicaid.provider_spending_staging p
+JOIN nppes.npi_staging n
+    ON p.servicing_npi = n.npi
+WHERE p.hcpcs_code LIKE 'D%'
+LIMIT 100;
+
+
+CREATE TABLE medicaid.provider_spending_enriched (
+    servicing_npi VARCHAR(10),
+    billing_npi VARCHAR(10),
+    hcpcs_code VARCHAR(10),
+    year_month TEXT,
+    beneficiaries_served_count INTEGER,
+    claims_count INTEGER,
+    total_amount_paid NUMERIC(18,2),
+
+    practice_address_line1 VARCHAR(55),
+    practice_address_line2 VARCHAR(55),
+    city VARCHAR(40),
+    state VARCHAR(40),
+    postal_code VARCHAR(20),
+    country_code VARCHAR(2),
+    phone VARCHAR(20)
+);
+
+
+
+CREATE INDEX idx_spending_servicing_npi
+ON medicaid.provider_spending_staging(servicing_npi);
+
+CREATE INDEX idx_npi_staging_npi
+ON nppes.npi_staging(npi);
+
+ANALYZE medicaid.provider_spending_staging;
+ANALYZE nppes.npi_staging;
+
+INSERT INTO medicaid.provider_spending_enriched
+SELECT
+    p.servicing_npi,
+    p.billing_npi,
+    p.hcpcs_code,
+    p.year_month,
+    p.beneficiaries_served_count,
+    p.claims_count,
+    p.total_amount_paid,
+
+    n.provider_first_line_business_practice_location_address,
+    n.provider_second_line_business_practice_location_address,
+    n.provider_business_practice_location_address_city_name,
+    n.provider_business_practice_location_address_state_name,
+    n.provider_business_practice_location_address_postal_code,
+    n.provider_business_practice_location_address_country_code,
+    n.provider_business_practice_location_address_telephone_number
+FROM medicaid.provider_spending_staging p
+JOIN nppes.npi_staging n
+  ON p.servicing_npi = n.npi::text
+WHERE p.hcpcs_code LIKE 'D%';
+
